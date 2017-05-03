4d1fea464682111dca2a631b3b8e9b003323a2d0
michelesp <spina93michel@gmail.com>
2017-05-03 20:44:14 +0200

    lempel ziv algorithm

--- classes/composition.php
+++ classes/composition.php
@@ -4,6 +4,8 @@ class SOMUSIC_CLASS_Composition implements Serializable, JsonSerializable {
 	private $id;
 	private $user_c;
 	private $timestamp_c;
+	private $stepValue;
+	private $accidentalValue;
 	public $name;
 	public $user_m;
 	public $timestamp_m;
@@ -20,6 +22,8 @@ class SOMUSIC_CLASS_Composition implements Serializable, JsonSerializable {
 		$this->timestamp_m = $timestamp_m;
 		$this->instrumentsScore = $instrumentsScore;
 		$this->instrumentsUsed = $instrumentsUsed;
+		$this->stepValue = array("A"=>5, "B"=>6, "C"=>0, "D"=>1, "E"=>2, "F"=>3, "G"=>4);
+		$this->accidentalValue = array("b"=>-1, "clear"=>0, "#"=>1);
 	}
 	
 	
@@ -46,7 +50,138 @@ class SOMUSIC_CLASS_Composition implements Serializable, JsonSerializable {
 	}
 	
 	public function jsonSerialize () {
-		return get_object_vars($this);
+		$vars = get_object_vars($this);
+		unset($vars["stepValue"]);
+		unset($vars["accidentalValue"]);
+		return $vars;
+	}
+	
+	public function getMelodicRepresentation($scoreIndex) {
+		$modalNote = $this->findModalNote($scoreIndex);
+		$melodicRepresentation = "";
+		$measures = $this->instrumentsScore[$scoreIndex]->measures;
+		for($i=0; $i<count($measures); $i++) {
+			$voice = $measures[$i]->voices[0];			//TODO: extends to all voices
+			for($j=0; $j<count($voice); $j++) {
+				$note = $voice[$j];
+				if(!$note->isRest) {
+					$noteArr = array("octave"=>$note->octave[0], "step"=>$note->step[0], "accidental"=>(isset($note->accidental[0])?$note->accidental[0]:"clear"));
+					$noteRepresentation = $this->getNoteDistance($noteArr, $modalNote);
+					$melodicRepresentation.=($noteRepresentation>=0?'+'.$noteRepresentation:$noteRepresentation);
+				}
+				else if(strlen($melodicRepresentation)==0 || $melodicRepresentation[strlen($melodicRepresentation)-1]!='p')
+					$melodicRepresentation.='p';
+			}
+		}
+		return $melodicRepresentation;
+	}
+	
+	public function getMelodicRepresentation2($scoreIndex) {
+		$melodicRepresentation = "";
+		$measures = $this->instrumentsScore[$scoreIndex]->measures;
+		$lastNote = null;
+		for($i=0; $i<count($measures); $i++) {
+			$voice = $measures[$i]->voices[0];			//TODO: extends to all voices
+			for($j=0; $j<count($voice); $j++) {
+				$note = $voice[$j];
+				if(!$note->isRest) {
+					$noteArr = array("octave"=>$note->octave[0], "step"=>$note->step[0], "accidental"=>(isset($note->accidental[0])?$note->accidental[0]:"clear"));
+					if(isset($lastNote))
+						$noteRepresentation = $this->getNoteDistance($noteArr, $lastNote);
+					else $noteRepresentation = 0;
+					$melodicRepresentation.=($noteRepresentation>=0?'+'.$noteRepresentation:$noteRepresentation);
+					$lastNote = $noteArr;
+				}
+				else if(strlen($melodicRepresentation)==0 || $melodicRepresentation[strlen($melodicRepresentation)-1]!='p')
+					$melodicRepresentation.='p';
+			}
+		}
+		return $melodicRepresentation;
+	}
+	
+	private function getNoteDistance($note, $modal) {
+		$distance = 0;
+		$minMax = $this->getMinMaxNotes($note, $modal);
+		$note0 = $minMax[0];
+		//var_dump(array($note, $minMax[1]));
+		while($note0["octave"]!=$minMax[1]["octave"] || $note0["step"]!=$minMax[1]["step"]) {
+			if($note0["step"]=="B" || $note0["step"]=="E")
+				$distance++;
+			else $distance+=2;
+			$stepValue = $this->stepValue[$note0["step"]];
+			if($stepValue==6) {
+				$note0["step"] = "C";
+				$note0["octave"]++;
+			}
+			else $note0["step"] = array_keys($this->stepValue, $stepValue+1)[0];
+		}
+		$distance-=$this->accidentalValue[$minMax[0]["accidental"]];
+		$distance+=$this->accidentalValue[$minMax[1]["accidental"]];
+		return ($minMax[0]==$note?-1*$distance:$distance);
+	}
+	
+	private function getMinMaxNotes($note1, $note2) {
+		if($note1["octave"]<$note2["octave"])
+			$minNote = $note1;
+		else if($note1["octave"]>$note2["octave"])
+			$minNote = $note2;
+		else {
+			if($this->stepValue[$note1["step"]]<$this->stepValue[$note2["step"]])
+				$minNote = $note1;
+			else if($this->stepValue[$note1["step"]]>$this->stepValue[$note2["step"]])
+				$minNote = $note2;
+			else {
+				if($this->accidentalValue[$note1["accidental"]]<$this->accidentalValue[$note2["accidental"]])
+					$minNote = $note1;
+				else $minNote = $note2;
+			}
+		}
+		return array($minNote, ($minNote==$note1?$note2:$note1));
+	}
+	
+	private function findModalNote($scoreIndex) {
+		$measures = $this->instrumentsScore[$scoreIndex]->measures;
+		$occurrences = array();
+		for($i=0; $i<count($measures); $i++) {
+			$voice = $measures[$i]->voices[0];			//TODO: extends to all voices
+			for($j=0; $j<count($voice); $j++) {
+				$note = $voice[$j];
+				if(!$note->isRest) {
+					$noteArr = array("octave"=>$note->octave[0], "step"=>$note->step[0], "accidental"=>(isset($note->accidental[0])?$note->accidental[0]:"clear"));
+					$noteName = json_encode($noteArr);
+					if(isset($occurrences[$noteName]))
+						$occurrences[$noteName]++;
+					else $occurrences[$noteName] = 0;
+				}
+			}
+		}
+		if(count($occurrences)==0) {
+			$this->melodicRepresentation[$scoreIndex] = "p";
+			return null;
+		}
+		return $this->getLowerNoteString(array_keys($occurrences, max($occurrences)));
+	}
+	
+	private function getLowerNoteString($notesString) {
+		$notes = array();
+		$minOctave = INF;
+		foreach ($notesString as $noteString) {
+			$note = json_decode($noteString);
+			array_push($notes, $note);
+			if($note->octave<$minOctave)
+				$minOctave = $note->octave;
+		}
+		$minStep = "B";
+		foreach ($notes as $note) {
+			if($minOctave==$note->octave && $this->stepValue[$note->step]<$this->stepValue[$minStep])
+				$minStep = $note->step;
+		}
+		$minAccidental = "#";
+		foreach ($notes as $note) {
+			if($minOctave==$note->octave && $minStep==$note->step && $this->accidentalValue[$note->accidental]<$this->accidentalValue[$minAccidental])
+				$minAccidental = $note->accidental;
+		}
+		return array("octave"=>$minOctave, "step"=>$minStep, "accidental"=>$minAccidental);
 	}
 	
 }
---
+++ classes/lzw.php
@@ -0,0 +1,88 @@
+<?php
+
+class SOMUSIC_CLASS_LZW {
+	private $dictionary;
+	private $invDictionary;
+	
+	public function __construct() {
+		$this->dictionary = array();
+		$this->dictionary[0] = "";
+		$this->invDictionary = array();
+		$this->invDictionary[""] = 0;
+	}
+	
+	public function compress($string){
+		$data = "";
+		$start = 0;
+		$couples = array();
+		$nTokens = 0;
+		$str = "";
+		$lastIndex = 0;
+		$token = $this->nextToken($string, $start);
+		$start += strlen($token);
+		while(!is_bool($token)) {
+			$nTokens++;
+			$str.=$token;
+			if(!isset($this->invDictionary[$str])) {
+				$index = count($this->dictionary);
+				$this->dictionary[$index] = $str;
+				$this->invDictionary[$str] = $index;
+				array_push($couples, array($lastIndex, $token));
+				$p = pack("SZ*", $lastIndex, $token);
+				$data.=$p;
+				$str = "";
+				$lastIndex = 0;
+			}
+			else $lastIndex = $this->invDictionary[$str];
+			$token = $this->nextToken($string, $start);
+			$start += strlen($token);
+		}
+		if(strlen($str)>0) {
+			array_push($couples, array($lastIndex, null));
+			$p = pack("SZ*", $lastIndex, "ppp");
+			$data.=$p;
+		}
+		return $data;
+	}
+	
+	public function decode($data) {
+		$dictionary = array();
+		$dictionary[0] = "";
+		$output = "";
+		$lenData = strlen($data);
+		$i = 0;
+		while($i<$lenData) {
+			$index = unpack("S", substr($data, $i, 2))[1];
+			$i += 2;
+			$str = "";
+			$ch = unpack("C", substr($data, $i, 1))[1];
+			$i++;
+			while($ch!=0) {
+				$str.=chr($ch);
+				$ch = unpack("C", substr($data, $i, 1))[1];
+				$i++;
+			}
+			while(strlen($str)>1 && $str[0]==$str[1])
+				$str = substr($str, 1);
+			$str1 = $dictionary[$index].$str;
+			$output .= $str1;
+			$dictionary[count($dictionary)] = $str1;
+		}
+		return substr($output, 0, -1);
+	}
+
+	private function nextToken($string, $start) {
+		$len = strlen($string);
+		if($len<=$start)
+			return false;
+		$token = $string[$start];
+		for($i=$start+1; $i<$len && is_numeric($string[$i]); $i++) {
+			$token.=$string[$i];
+		}
+		return $token;
+	}
+
+
+}
+
+?>
